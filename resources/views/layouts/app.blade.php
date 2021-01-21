d02c7a8890b1a7ea2fa3147d08b136b1a096034d
Sabrina <sabrinacunsolo85@gmail.com>
2021-01-21 10:12:49 +0100

    inserita favicon tavolozza

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Art Shop</title>
+        {{-- favicon --}}
+        <link rel="shortcut icon" href="https://img.icons8.com/plasticine/2x/paint-palette.png">
         <!-- Style -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
