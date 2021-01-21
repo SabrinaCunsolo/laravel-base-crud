b9091acabd17a9223aba9523d8cbce65c208fe0a
Sabrina <sabrinacunsolo85@gmail.com>
2021-01-21 09:30:00 +0100

    cambiando stile navbar bootstrap funziona

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+{{-- <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
     <ul class="navbar-nav">
         <li class="nav-item active">
             <a class="nav-link" href="{{ route('homepage') }}">
@@ -12,4 +12,25 @@
             </a>
         </li>
     </ul>
-</div>
+</div> --}}
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-md-center">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('homepage') }}">
+                    Homepage
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('products.index') }}">
+                    Products
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
